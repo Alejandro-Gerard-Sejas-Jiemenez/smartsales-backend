863bc0750af0f3bcf5db11476412e90ad8f6c7ab
Alejandro-Gerard-Sejas-Jiemenez <alejandrogerards@gmail.com>
2025-11-13 00:42:51 -0400

    Remove firebase key from index and ignore firebase keys

--- apps/venta_transacciones/tests.py
+++ apps/venta_transacciones/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class CarritoTests(TestCase):
+	"""Minimal placeholder tests restored after revert."""
+	def test_placeholder(self):
+		self.assertTrue(True)
+
